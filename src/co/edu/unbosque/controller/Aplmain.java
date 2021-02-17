0bd6401aedaa68fb81f9c819ddbbf8477dd55610
cdsanchezm <64279165+cdsanchezm@users.noreply.github.com>
2021-02-17 16:29:43 -0500

    new branch

--- src/co/edu/unbosque/controller/Aplmain.java
+++ src/co/edu/unbosque/controller/Aplmain.java
@@ -6,6 +6,8 @@ public class Aplmain {
 		System.out.println("hola mundo");
 		System.out.println("andres nos robo en las votaciones");
 		
+		System.out.println("esteban es la peor secretaria");
+		
 	}
 
 }
