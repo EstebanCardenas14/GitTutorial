428e5bdc2f47e71f1f445d68d0eeb1af1c41d859
cdsanchezm <64279165+cdsanchezm@users.noreply.github.com>
2021-02-17 16:24:05 -0500

    Update Aplmain.java

--- src/co/edu/unbosque/controller/Aplmain.java
+++ src/co/edu/unbosque/controller/Aplmain.java
@@ -4,7 +4,7 @@ public class Aplmain {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo");
-		system.out.println("andres nos robo en las votaciones");
+		System.out.println("andres nos robo en las votaciones");
 		
 	}
 
